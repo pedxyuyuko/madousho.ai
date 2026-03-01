3e166cb278b36df2b591147c029412da3dd884f4
SaigyoujiYuyuko <uuz@sakurauuz.moe>
2026-02-28 23:50:10 -0500

    fix: remove show_config_cmd

--- src/madousho/commands/__init__.py
+++ src/madousho/commands/__init__.py
@@ -2,6 +2,5 @@
 
 from .run import run_cmd
 from .validate import validate_cmd
-from .show_config import show_config_cmd
 
-__all__ = ["run_cmd", "validate_cmd", "show_config_cmd"]
+__all__ = ["run_cmd", "validate_cmd"]
