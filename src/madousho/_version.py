8502a51e9ee0e643c07d592dd831d35a6670d9cd
SaigyoujiYuyuko <uuz@sakurauuz.moe>
2026-03-01 00:17:19 -0500

    fix: remove local version identifier for PyPI compatibility
    - Change local_scheme from 'node-and-date' to 'no-local-version'
    - PyPI/TestPyPI reject packages with local versions (+HASH part)
    - New version format: 0.0.1.dev19 (PEP 440 compliant)

--- src/madousho/_version.py
+++ src/madousho/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.1.dev18+g3e166cb27.d20260301'
-__version_tuple__ = version_tuple = (0, 0, 1, 'dev18', 'g3e166cb27.d20260301')
+__version__ = version = '0.0.1.dev19'
+__version_tuple__ = version_tuple = (0, 0, 1, 'dev19')
 
-__commit_id__ = commit_id = 'g3e166cb27'
+__commit_id__ = commit_id = 'g59fb3ba5f'
